0a9ccefe94aea7b6bdaeb441ad9ca39ef8533de8
Reyna Feng <98496844+reyna-feng@users.noreply.github.com>
2022-04-27 13:43:59 -0700

    Update asset_download.sql

--- asset_download.sql
+++ asset_download.sql
@@ -1,3 +1,4 @@
+--Update Time: 4/27
 CREATE OR REPLACE TABLE `unity-other-learn-prd.reynafeng.asset_download` AS
 
 WITH asset AS(
@@ -7,21 +8,15 @@ WITH asset AS(
 )
 
 SELECT *,
-       COUNT(*) OVER(PARTITION BY compliance_key,sp,egl,ep) AS total_downloads
+       COUNT(*) OVER(PARTITION BY compliance_key) AS total_downloads
 FROM(
 SELECT body.ts AS timestamp,A.submit_date,A.compliance_key,
        IF(NOT name IS NULL, name, 'Unknown') AS asset_name,
        IF(NOT publisher_name IS NULL, publisher_name, 'Unknown') AS publisher_name,
-       IF(NOT B.compliance_key IS NULL, True, False) AS sp,
-       IF(NOT B.license IS NULL, B.license, 'Not Applied') AS license,
-       IF(NOT C.user_id IS NULL, True, False) AS egl,
-       IF(NOT E.compliance_key IS NULL, True, False) AS ep,
-       IF(NOT E.license IS NULL, E.license, 'Not Applied') AS educator_license
+       B.* EXCEPT(compliance_key)
 FROM `unity-ai-data-prd.assetStore_storeFront.assetStore_storeFront_packageEditorDownload_v1` A
+JOIN `unity-other-learn-prd.reynafeng.academiclicense` B ON A.compliance_key=B.compliance_key AND A.submit_date BETWEEN DATE(B.grant_time) AND DATE(B.expire_time)
 LEFT JOIN asset D ON SAFE_CAST(A.body.package_id AS INT64)=D.id
-LEFT JOIN `unity-other-learn-prd.reynafeng.student_activation` B ON A.compliance_key=B.compliance_key AND A.submit_date BETWEEN LEAST(DATE(B.licnese_create_time),DATE(B.licnese_grant_time)) AND DATE(B.licnese_expiration_time)
-LEFT JOIN `unity-other-learn-prd.reynafeng.egl_grant_license` C ON A.compliance_key=C.user_id AND A.submit_date BETWEEN C.grant_time AND C.expire_time
-LEFT JOIN `unity-other-learn-prd.reynafeng.educator_activation` E ON A.compliance_key=E.compliance_key AND A.submit_date BETWEEN LEAST(DATE(E.licnese_create_time),DATE(E.licnese_grant_time)) AND DATE(E.licnese_expiration_time)
 WHERE submit_date IS NOT NULL
-GROUP BY 1,2,3,4,5,6,7,8,9,10
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
 ) AS A 
