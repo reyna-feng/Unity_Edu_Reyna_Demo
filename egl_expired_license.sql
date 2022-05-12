c1c6704b15cc1ca9ec863d868a785d923112e0c0
reyna-feng <reyna.feng@unity3d.com>
2022-05-12 10:04:55 -0500

    commit my local change

---
+++ egl_expired_license.sql
@@ -0,0 +1,14 @@
+--Update time: 5/3
+CREATE OR REPLACE VIEW `unity-other-learn-prd.reynafeng.egl_expired_license` AS
+
+SELECT license,institutionName,start_month,end_month,contactEmail,
+       DATE_DIFF(end_month, start_month, month) AS duration_month
+FROM(
+SELECT license_record_id ,license ,institutionName,request_time,expire_time,contactEmail,
+       MIN(DATE_TRUNC(request_time, month)) OVER(PARTITION BY license_record_id) AS start_month,
+       MAX(DATE_TRUNC(expire_time, month)) OVER(PARTITION BY license_record_id) AS end_month
+FROM `unity-other-learn-prd.reynafeng.egl_grant_license`
+WHERE status='Approved'
+GROUP BY 1,2,3,4,5,6) AS A
+WHERE end_month <= DATE_SUB(DATE_TRUNC(current_date,month), INTERVAL 1 MONTH)
+ORDER BY 4 DESC
